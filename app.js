b798b0faa634001b6fb0ccefdb803929210e7f8e
Jonathan Jackson <jack0355@algonquinlive.com>
2019-02-01 16:00:16 -0500

    organizing files and adding a-frame and app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+//Setup for app
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+//convenience vars
+const LISTEN_PORT = 8080;
+
+//middleware
+app.use(express.static(__dirname + '/public'));
+
+//set a route
+app.get('/', function(req, res){
+    res.sendFile(__dirname + 'public/index.html');
+});
+
+server.listen(LISTEN_PORT);
+console.log("listening on port: " + LISTEN_PORT);
