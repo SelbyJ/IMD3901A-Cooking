54b47dd3b193ebbed571ea7e639a881c83a967aa
Leyalee Alsaadi <alsa0162@algonquinlive.com>
2019-02-06 08:27:52 -0500

    updated app.js

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
+const server = require('http').createServer(app);
 
 //convenience vars
 const LISTEN_PORT = 8080;
 
 //middleware
+server.listen(LISTEN_PORT);
 app.use(express.static(__dirname + '/public'));
+console.log("Listening on port: " + LISTEN_PORT);
 
 //set a route
 app.get('/', function(req, res){
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 server.listen(LISTEN_PORT);
