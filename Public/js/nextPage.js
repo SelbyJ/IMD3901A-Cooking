553e545a41f2633873f429829d443c0f06a96b09
Leyya Alsaadi <leyaleealsaadi@cmail.carleton.ca>
2019-02-06 20:31:13 -0500

    trying to add animations in javascript

--- Public/js/nextPage.js
+++ Public/js/nextPage.js
@@ -1,4 +1,17 @@
 function openBook(){
     var cover = document.getElementById("bookCover");
-    cover.style.transform = "rotateY(-180deg)";
+    var degrees = 0;
+    var interval = setInterval(frame, 10);
+    
+    function frame(){
+        if(degrees == -180){
+            clearInterval(degrees);
+        }else{
+            degrees+= -18;
+            cover.style.transform = "rotateY(" + degrees.toString() + ")";
+        }
+        
+    }
+
+    
 }
