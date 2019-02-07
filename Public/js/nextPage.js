d56033c49ea28b52ac7cb1beb5677ae64e2d7df2
Leyya Alsaadi <leyaleealsaadi@cmail.carleton.ca>
2019-02-06 22:27:14 -0500

    added page flip to multiple pages

--- Public/js/nextPage.js
+++ Public/js/nextPage.js
@@ -9,11 +9,21 @@ function openBook(){
         }else{
             degrees+= -18;
             cover.style.transform = "rotateY(" + degrees + "deg)";
-        }
-        
-        
-        
+        }   
     }
+}
 
+function nextPage(){
+    var page = document.getElementById("page");
+    var degrees = 0;
+    var interval = setInterval(frame, 10);
     
+    function frame(){
+        if(degrees == -180){
+            clearInterval(interval);
+        }else{
+            degrees+= -18;
+            page.style.transform = "rotateY(" + degrees + "deg)";
+        }   
+    }
 }
