6e9ac4f3ca64f71379bd34923ade46f0830496d4
Leyya Alsaadi <leyaleealsaadi@cmail.carleton.ca>
2019-02-11 23:15:02 -0500

    trying to add page flip animation

--- Public/js/front-flip.js
+++ Public/js/front-flip.js
@@ -7,14 +7,16 @@ AFRAME.registerComponent('front-flip', {
 
         object.el.addEventListener('click', function(event){
             if(object.data.pageNum === 'cover'){
-                let page = document.querySelector('#cover')
+                let page = document.querySelector('#cover');
+                page.setAttribute('animation', {property: 'rotation', dir: 'alternate', dur: 1000, easing: 'easeInSine', loop: 'true', to: {x:0, y:-180, z:0}});
+
             }else if(object.data.pageNum === 'one'){
-                let page = document.querySelector('#page-1')
+                let page = document.querySelector('#one')
             }else if(object.data.pageNum === 'two'){
-                let page = document.querySelector('#page-2')
+                let page = document.querySelector('#two')
             }else if(object.data.pageNum === 'three'){
-                let page = document.querySelector('#page-3')
+                let page = document.querySelector('#three')
             }
-        })
+        });
     }
-})
+});
