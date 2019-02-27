bfbc84c097d1b71e15cc7ee0ea17dae7889458c8
Jonathan Jackson <jack0355@algonquinlive.com>
2019-02-27 09:05:27 -0500

    index.html changed to kitchen.html

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 
 //set a route
 app.get('/', function(req, res){
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + 'public/kitchen.html');
 });
 
 server.listen(LISTEN_PORT);
