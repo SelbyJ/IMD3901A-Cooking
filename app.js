f9b3609586b5e2b290a23f7555af653946d913f3
jonathan Jackson <jonathangjackson@cmail.carleton.ca>
2019-03-04 13:23:06 -0500

    before the merge

--- app.js
+++ app.js
@@ -4,15 +4,37 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+const socketIO = require('socket.io')(server);
+
 //convenience vars
 const LISTEN_PORT = 8080;
-
+/*
 //middleware
 app.use(express.static(__dirname + '/public'));
 
 //set a route
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/Public/vr.html');
+    res.sendFile(__dirname + '/Public/kitchen.html');
+});
+*/
+app.get('/kitchen', function(req, res) {
+    app.use(express.static(__dirname + '/public'));
+    res.sendFile(__dirname + '/Public/kitchen.html');
+});
+
+/*
+app.get('/desktop', function(req, res) {
+    app.use(express.static('Public/assets'));
+    res.sendFile(__dirname + '/public/desktop.html');
+});
+*/
+
+socketIO.on('connection', function(socket){
+    console.log(socket.id + " has connected!");
+
+    socket.on('disconnect', function(data){
+        console.log(socket.id + " has disconnected");
+    });
 });
 
 server.listen(LISTEN_PORT);
