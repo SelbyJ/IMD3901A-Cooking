5caeb47cf2e0ed89c3e631df171ddd3aeedc44f8
Leyalee Alsaadi <alsa0162@algonquinlive.com>
2019-03-08 13:26:42 -0500

    testing start page

--- Public/js/start-page.js
+++ Public/js/start-page.js
@@ -13,20 +13,20 @@ document.querySelector('#vrUser').addEventListener('click', function(){
     console.log('VR User Selected');
     socket.emit('vrclicked');
     vr = true;
-    window.location.href = '/kitchen.html';
+    window.location.href = '/kitchen';
 });
     
 document.querySelector('#mobileUser').addEventListener('click', function(){
     console.log('Mobile User Selected');
     socket.emit('mobileclicked');
     mobile = true;
-    window.location.href = '/mobile.html';
+    window.location.href = '/mobile';
 });
 
 if(vr == true){
-    window.location.href = '/start_page.html';
+    window.location.href = '/start_page';
 }
 
 if(mobile == true){
-    window.location.href('/start_page.html');
+    window.location.href = '/start_page';
 }
